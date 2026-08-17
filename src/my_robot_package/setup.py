512f988827c2530fa0e0d988807f02bf5a28ec3e
harshcshah97-collab <harshcshah97@gmail.com>
2026-08-17 18:04:05 -0400

    Ignore generated directories during package install

--- src/my_robot_package/setup.py
+++ src/my_robot_package/setup.py
@@ -4,6 +4,12 @@ from glob import glob
 
 package_name = 'my_robot_package'
 
+
+def regular_files(pattern):
+    """Return only files so generated directories cannot break packaging."""
+    return [path for path in glob(pattern) if os.path.isfile(path)]
+
+
 setup(
     name=package_name,
     version='0.1.0',
@@ -12,11 +18,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
-        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
-        (os.path.join('share', package_name, 'web_ui'), glob('web_ui/*')),
+        (os.path.join('share', package_name, 'launch'),
+            regular_files('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'),
+            regular_files('config/*.yaml')),
+        (os.path.join('share', package_name, 'urdf'),
+            regular_files('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'maps'),
+            regular_files('maps/*')),
+        (os.path.join('share', package_name, 'web_ui'),
+            regular_files('web_ui/*')),
     ],
     install_requires=[
         'setuptools',
@@ -29,7 +40,9 @@ setup(
     zip_safe=True,
     maintainer='Harsh Shah',
     maintainer_email='harshcshah97@gmail.com',
-    description='ROS 2 companion robot mapping, navigation, and visual tracking',
+    description=(
+        'ROS 2 companion robot mapping, navigation, and visual tracking'
+    ),
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
@@ -42,11 +55,13 @@ setup(
             'wall_follower = my_robot_package.wall_follower:main',
             'bumper_node = my_robot_package.bumper_node:main',
             'assistant_node = my_robot_package.assistant_node:main',
-            'person_follower_node = my_robot_package.person_follower_node:main',
+            'person_follower_node = '
+            'my_robot_package.person_follower_node:main',
             'check_vision = my_robot_package.check_vision:main',
             'oakd_ai_test = my_robot_package.oakd_ai_test:main',
             'calibration_check = my_robot_package.calibration_check:main',
-            'perception_benchmark = my_robot_package.perception_benchmark:main',
+            'perception_benchmark = '
+            'my_robot_package.perception_benchmark:main',
         ],
     },
 )
